c7d1740031225b4c0c195ff39461952c0371c332
eduardo-soto1997 <50255822+eduardo-soto1997@users.noreply.github.com>
2020-02-23 18:41:32 -0600

    Fixed Some of the front end stuff and made it even more SASSy

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,29 +10,24 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/5b190a499f.js" crossorigin="anonymous"></script>
     <title>@yield('title')</title>
 </head>
 
 <body>
   <nav id="main-nav">
-    <div class="btn-group dropright">
-  <button type="button" class="btn btn-secondary">
-      <a href="/"> Home </a>
-  </button>
-  <button type="button" id="dropdownMenuButton" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <span class="sr-only">Toggle Dropright</span>
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    @if(Auth::user()->is_Admin)
-      <a href="/classes"> Classes </a>
-      <a href ="/students"> Students </a>
-      <a href="/patients"> Patients </a>
-    @endif
-    <a class="btn btn-primary" href="{{ url('/logout') }}" role="button"> logout </a>
-  </div>
-</div>
+    <div class="wrapper">
+      <ul>
+        <li><a href="/"> Home </a></li>
+        @if(Auth::user()->is_Admin)
+        <li><a href="/classes"> Classes </a></li>
+        <li><a href ="/students"> Students </a></li>
+        <li><a href="/patients"> Patients </a></li>
+        @endif
+        <li><a href="{{ url('/logout') }}" role="button"> logout </a></li>
+      </ul>
+    </div>
   </nav>
-
 
 @yield('content')
 </body>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Nursing Simulation - Login</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Aurora University
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
